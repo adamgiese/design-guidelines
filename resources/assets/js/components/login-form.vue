013d1c6b1b4cdb0965f6a8a81182ed03b2d365f8
Adam Giese <adam.giese@gmail.com>
2017-01-28 14:56:18 -0600

    Add registration

--- resources/assets/js/components/login-form.vue
+++ resources/assets/js/components/login-form.vue
@@ -5,6 +5,22 @@
     <span v-if="user" class="login-form-button" v-on:click="logout">Logout</span>
     <div v-if="loginActive" v-on:click="toggleLogin" class="login-form-overlay"></div>
     <div v-if="loginActive" class="login-form-container">
+      <h2 class="login-form-title">Register</h2>
+      <label class="login-form-item">
+        <span class="login-form-label">Full Name</span>
+        <input v-model="registerName" class="login-form-input"/>
+      </label>
+      <label class="login-form-item">
+        <span class="login-form-label">Email</span>
+        <input v-model="registerEmail" class="login-form-input"/>
+      </label>
+      <label class="login-form-item">
+        <span class="login-form-label">Password</span>
+        <input type="password" v-model="registerPassword" class="login-form-input"/>
+        <input type="password" v-model="registerPasswordConfirm" class="login-form-input"/>
+      </label>
+      <button v-on:click="register">Login Now!</button>
+      <h2 class="login-form-title">Login</h2>
       <label class="login-form-item">
         <span class="login-form-label">Email</span>
         <input v-model="email" class="login-form-input"/>
@@ -23,12 +39,23 @@ export default {
   data: function() {
     return {
       'loginActive': false,
+      'registerName': '', 
+      'registerEmail': '', 
+      'registerPassword': '',
       'email': '', 
       'password': '',
       'user': false,
     }
   },
   computed: {
+    registerData: function() {
+      return {
+        "name": this.registerName,
+        "email": this.registerEmail,
+        "password": this.registerPassword,
+        "password_confirmation": this.registerPasswordConfirm,
+      }
+    },
     loginData: function() {
       return {
         "email": this.email,
@@ -47,11 +74,21 @@ export default {
     toggleLogin: function() {
       this.loginActive = !this.loginActive;
     },
+    register: function() {
+      this.$http.post('/register', this.registerData).then((response) => {
+        console.log(response);
+        window.location.reload();
+        //success
+      }, (response) => {
+        console.log(response);
+        //failure
+      });
+    },
     login: function() {
       this.$http.post('/api/v1/authenticate', this.loginData).then((response) => {
         //success
         this.user = response.body.user;
-        console.log(response.body.user);
+        console.log(response.body);
         this.loginActive = false;
       }, (response) => {
         //failure
